8477637e0cfcce397200e6c8f7d75000cdb20246
Amir <rezaamir731@gmail.com>
2024-08-22 23:12:34 +0330

    feat: -base62_encode

---
+++ backend/platform/database/migrations/002_encode_base62.sql
@@ -0,0 +1,24 @@
+-- +goose Up
+CREATE OR REPLACE FUNCTION base62_encode(num BIGINT) RETURNS TEXT AS
+$$
+DECLARE
+    alphabet  TEXT    := '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_';
+    base      INTEGER := 62;
+    encoded   TEXT    := '';
+    remainder INTEGER;
+BEGIN
+    IF num = 0 THEN
+        RETURN SUBSTR(alphabet, 1, 1);
+    END IF;
+    WHILE num > 0
+        LOOP
+            remainder := (num % base) + 1;
+            encoded := SUBSTR(alphabet, remainder, 1) || encoded;
+            num := num / base;
+        END LOOP;
+    RETURN encoded;
+END;
+$$ LANGUAGE plpgsql;
+
+-- +goose Down
+DROP FUNCTION base62_encode(num BIGINT);
