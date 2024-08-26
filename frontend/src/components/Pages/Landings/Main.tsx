d9098ac694a0fc087527ec992f603d5e96b12066
Amirreza <rezaamir731@gmail.com>
2024-08-26 21:07:31 +0330

    feat: -add dialog to navigate to proper section

--- frontend/src/components/Pages/Landings/Main.tsx
+++ frontend/src/components/Pages/Landings/Main.tsx
@@ -1,13 +1,17 @@
-import React, { type FC } from "react";
+import React, { type FC, useState } from "react";
 import { Spotlight } from "@/components/AnimatedSections/SpotLight.tsx";
 import { FlipWords } from "@/components/AnimatedSections/FlipWords.tsx";
 import { AuroraBackground } from "@/components/AnimatedSections/AuroraBackground.tsx";
 import { motion } from "framer-motion";
 import { Button } from "@UI/button.tsx";
+import { Dialog, DialogContent, DialogDescription } from "@UI/dialog.tsx";
 
 const MainLanding: FC = () => {
+	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+	const openModal = () => setIsModalOpen(true);
+
 	return (
-		<div className={"snap-y snap-mandatory"}>
+		<div className={"snap-y snap-mandatory overflow-auto h-[100svh]"}>
 			<div className="snap-center h-[100svh] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
 				<Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill="white" />
 				<div className="h-[40rem] flex justify-center items-center px-4">
@@ -20,7 +24,7 @@ const MainLanding: FC = () => {
 			</div>
 			<AuroraBackground
 				containerProps={{
-					className: "snap-center",
+					className: "snap-center h-[100svh]",
 				}}
 			>
 				<motion.div
@@ -37,11 +41,44 @@ const MainLanding: FC = () => {
 					<div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
 						It's free, easy and secure. Also, no ads.
 					</div>
-					<Button variant={"default"} className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
+					<Button
+						onClick={openModal}
+						variant={"default"}
+						className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2"
+					>
 						Start Now
 					</Button>
 				</motion.div>
 			</AuroraBackground>
+
+			{/* Dialogs */}
+			<Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+				<DialogContent>
+					<DialogDescription asChild>
+						<motion.p
+							className={"text-white"}
+							initial={{ opacity: 0 }}
+							whileInView={{ opacity: 1 }}
+							transition={{ delay: 0.1, duration: 0.3 }}
+						>
+							You can use our service without any registration, but if you want to manage your links and use all the features you
+							will need to create an account.
+							<br />
+							What do you prefer to do?
+						</motion.p>
+					</DialogDescription>
+					<DialogDescription asChild className={"flex flex-row gap-4 items-center"}>
+						<motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.5, duration: 0.3 }}>
+							<Button asChild variant={"default"}>
+								<a href="/auth/register">Create an account.</a>
+							</Button>
+							<Button asChild className={"text-white"} variant={"outline"}>
+								<a href="/temp-urls/create">Start Now!</a>
+							</Button>
+						</motion.div>
+					</DialogDescription>
+				</DialogContent>
+			</Dialog>
 		</div>
 	);
 };
