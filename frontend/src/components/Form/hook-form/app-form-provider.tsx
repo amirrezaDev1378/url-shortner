84c0b72d7a05ca30a666ca2d93b9c2ae2268cb6e
Amir <rezaamir731@gmail.com>
2024-09-11 23:46:05 +0330

    feat: add preventDefault options to form provider

--- frontend/src/components/Form/hook-form/app-form-provider.tsx
+++ frontend/src/components/Form/hook-form/app-form-provider.tsx
@@ -1,18 +1,26 @@
 import { FormProvider as Form, type UseFormReturn } from "react-hook-form";
 import React from "react";
 
-// ----------------------------------------------------------------------
-
 type Props = {
 	children: React.ReactNode;
 	methods: UseFormReturn<any>;
-	onSubmit: VoidFunction;
+	onSubmit?: VoidFunction;
+	/**
+	 * This only works if onSubmit is not defined
+	 */
+	preventDefault?: boolean;
 } & React.HTMLAttributes<HTMLFormElement>;
 
-export default function AppFormProvider({ children, onSubmit, methods, ...formProps }: Props) {
+export default function AppFormProvider({ children, onSubmit, methods, preventDefault = true, ...formProps }: Props) {
+	const shouldPreventDefault = !onSubmit && preventDefault;
+	const onFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		if (shouldPreventDefault) return e.preventDefault();
+		if (typeof onSubmit === "function") return onSubmit();
+	};
+
 	return (
 		<Form {...methods}>
-			<form onSubmit={onSubmit} {...formProps}>
+			<form onSubmit={onFormSubmit} {...formProps}>
 				{children}
 			</form>
 		</Form>
