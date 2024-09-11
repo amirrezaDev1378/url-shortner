4367c62461b04ea13bd014ce266bc83c35c2b987
Amir <rezaamir731@gmail.com>
2024-09-11 23:52:27 +0330

    feat: add zod util function

--- frontend/src/lib/validation.ts
+++ frontend/src/lib/validation.ts
@@ -2,7 +2,11 @@ import { z } from "zod";
 
 export const ZodUrl = z.string().trim().min(1).url();
 
-export const zodEnumFromObject = <K extends string>(object: Record<K, any>): z.ZodEnum<[K, ...K[]]> => {
+export const zodEnumFromObjectKeys = <K extends string>(object: Record<K, any>): z.ZodEnum<[K, ...K[]]> => {
 	const [firstKey, ...otherKeys] = Object.keys(object) as K[];
+	return z.enum([firstKey, ...otherKeys]);
+};
+export const zodEnumFromObjectValues = <K extends string>(object: Record<K, any>): z.ZodEnum<[K, ...K[]]> => {
+	const [firstKey, ...otherKeys] = Object.values(object) as K[];
 	return z.enum([firstKey, ...otherKeys]);
 };
