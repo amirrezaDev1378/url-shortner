713f6ff830a5eda425ebbcdf56045e96f55ce2c7
Amir <rezaamir731@gmail.com>
2024-09-11 23:43:57 +0330

    revert: -handling animated tabs state from parent...

--- frontend/src/components/UI/AnimatedTabs.tsx
+++ frontend/src/components/UI/AnimatedTabs.tsx
@@ -1,41 +1,40 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { cn } from "@/lib/shadcn-utils.ts";
 
 export type Tab = {
 	title: string;
-	value: string;
+	value: any;
 };
 
 export const AnimatedTabs = ({
-	onTabChange,
 	tabs: propTabs,
 	containerClassName,
 	activeTabClassName,
 	tabClassName,
+	activeTab,
+	setActiveTab,
 }: {
-	onTabChange: (selected: any | null) => void;
 	tabs: Tab[];
+	activeTab: Tab;
+	setActiveTab: (tab: Tab) => void;
 	containerClassName?: string;
 	activeTabClassName?: string;
 	tabClassName?: string;
 }) => {
-	const [active, setActive] = useState<Tab>(propTabs[0]);
-
 	const moveSelectedTabToTop = (idx: number) => {
 		const newTabs = [...propTabs];
 		const selectedTab = newTabs.splice(idx, 1);
 		newTabs.unshift(selectedTab[0]);
-		setActive(newTabs[0]);
-		onTabChange(newTabs[0].value);
+		setActiveTab(newTabs[0]);
 	};
 
 	return (
 		<div
 			className={cn(
-				"flex flex-row items-center justify-start [perspective:1000px] relative overflow-auto sm:overflow-visible no-visible-scrollbar max-w-full w-full",
+				"no-visible-scrollbar relative flex w-full max-w-full flex-row items-center justify-start overflow-auto [perspective:1000px] sm:overflow-visible",
 				containerClassName
 			)}
 		>
@@ -45,18 +44,18 @@ export const AnimatedTabs = ({
 					onClick={() => {
 						moveSelectedTabToTop(idx);
 					}}
-					className={cn("relative px-4 py-2 rounded-full ", tabClassName)}
+					className={cn("relative rounded-full px-4 py-2", tabClassName)}
 					style={{
 						transformStyle: "preserve-3d",
 					}}
 				>
 					<AnimatePresence>
-						{active.value === tab.value && (
+						{activeTab?.value === tab.value && (
 							<motion.div
 								style={{ zIndex: -1 }}
 								layoutId="clickedbutton"
 								transition={{ type: "spring", bounce: 0.3, duration: 0.4 }}
-								className={cn("absolute inset-0 bg-gray-200 dark:bg-zinc-800 rounded-full ", activeTabClassName)}
+								className={cn("absolute inset-0 rounded-full bg-gray-200 dark:bg-zinc-800", activeTabClassName)}
 							/>
 						)}
 					</AnimatePresence>
