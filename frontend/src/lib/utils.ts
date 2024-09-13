6710a9c0af45c6aedfb1bc994e89977f052b8809
Amir <rezaamir731@gmail.com>
2024-09-13 23:28:42 +0330

    feat: add some utils to check if user has been created urls before

---
+++ frontend/src/lib/utils.ts
@@ -0,0 +1,11 @@
+import { createdFirstUrlKey } from "@/lib/constans.ts";
+
+export const userHasCreatedURlBefore = (): boolean => {
+	if (typeof window === "undefined") return false;
+	return localStorage.getItem(createdFirstUrlKey) === "true";
+};
+
+export const setUserHasCreatedURlBefore = (): void => {
+	if (typeof window === "undefined") return;
+	localStorage.setItem(createdFirstUrlKey, "true");
+};
