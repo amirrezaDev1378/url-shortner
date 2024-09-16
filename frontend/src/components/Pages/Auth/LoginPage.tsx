3279f3f9f02f55c4f9bdfdd5547c91aa6c370b86
Amir <rezaamir731@gmail.com>
2024-09-17 00:17:04 +0330

    feat: adjust auth ui

--- frontend/src/components/Pages/Auth/LoginPage.tsx
+++ frontend/src/components/Pages/Auth/LoginPage.tsx
@@ -5,13 +5,15 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@UI/button.tsx";
 import RHFTextInput from "@/components/Form/hook-form/rhf-text-input.tsx";
-import OAuthUI from "@/components/Pages/Auth/OAuth";
 import { initIdsRequest } from "@/lib/request.ts";
+import OAuthProviders from "@/components/Pages/Auth/OAuth";
+import { LuMail } from "react-icons/lu";
 
 const loginFormSchema = z.strictObject({
 	username: z.string().trim().min(1, { message: "invalid" }),
 	password: z.string().trim().min(1, { message: "invalid" }),
 });
+
 const LoginPage: FC = () => {
 	const form = useForm<z.infer<typeof loginFormSchema>>({
 		resolver: zodResolver(loginFormSchema),
@@ -45,42 +47,47 @@ const LoginPage: FC = () => {
 			withCredentials: true,
 		});
 	}, []);
+
 	return (
-		<div className="items-center justify-center flex w-full h-[100vh]">
-			<div className="rounded-2xl w-[300px] min-h-80 bg-background border-solid border-2 border-neutral-800">
-				<AppFormProvider className={"p-4 gap-4 flex-col flex"} methods={form} onSubmit={onSubmit}>
+		<div className="flex h-fit w-full items-center justify-center">
+			<div className="flex h-fit w-[300px] flex-col items-center rounded-2xl border-2 border-solid border-neutral-800 bg-background p-4">
+				<AppFormProvider className={"flex w-full flex-col gap-4"} methods={form} onSubmit={onSubmit}>
 					<RHFTextInput animatedInput animateError placeholder={"Username"} label={"Username"} name={"username"} />
 					<RHFTextInput animatedInput animateError placeholder={"Password"} label={"Password"} name={"password"} type={"password"} />
 
-					<Button type={"submit"}>submit</Button>
+					<Button type={"submit"} className={"flex flex-row items-center gap-2"}>
+						<LuMail />
+						Continue With Email
+					</Button>
 				</AppFormProvider>
+				<OAuthProviders />
 			</div>
-			<hr />
-			<p>Fix these styles</p>
-			<button
-				onClick={() => {
-					const cacelToken = new AbortController();
+			{/*<hr />*/}
+			{/*<p>Fix these styles</p>*/}
+			{/*<button*/}
+			{/*	onClick={() => {*/}
+			{/*		const cacelToken = new AbortController();*/}
 
-					initIdsRequest().post(
-						"https://127.0.0.1:3033/api/v1/urls/create",
-						{
-							slug: "s" + Math.round(Math.random() * 1000),
-							general_redirect_path: "https://deadsimplechat.com/blog/how-to-use-go-channels/",
-							ios_redirect_path: "https://time.ir",
-							type: "static",
-						},
-						{
-							signal: cacelToken.signal,
-						}
-					);
-					setTimeout(() => {
-						cacelToken.abort("fuck u");
-					}, 100);
-				}}
-			>
-				ass asdasd
-			</button>
-			<OAuthUI />
+			{/*		initIdsRequest().post(*/}
+			{/*			"https://127.0.0.1:3033/api/v1/urls/create",*/}
+			{/*			{*/}
+			{/*				slug: "s" + Math.round(Math.random() * 1000),*/}
+			{/*				general_redirect_path: "https://deadsimplechat.com/blog/how-to-use-go-channels/",*/}
+			{/*				ios_redirect_path: "https://time.ir",*/}
+			{/*				type: "static",*/}
+			{/*			},*/}
+			{/*			{*/}
+			{/*				signal: cacelToken.signal,*/}
+			{/*			}*/}
+			{/*		);*/}
+			{/*		setTimeout(() => {*/}
+			{/*			cacelToken.abort("fuck u");*/}
+			{/*		}, 100);*/}
+			{/*	}}*/}
+			{/*>*/}
+			{/*	ass asdasd*/}
+			{/*</button>*/}
+			{/*<OAuthUI />*/}
 		</div>
 	);
 };
