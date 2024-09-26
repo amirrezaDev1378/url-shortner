650915500261667c4037e8c1068031e7817d315d
Amir <rezaamir731@gmail.com>
2024-09-26 17:37:28 +0330

    feat: removed @vitejs/plugin-basic-ssl (incompatible with bun)

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,15 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
-import basicSsl from "@vitejs/plugin-basic-ssl";
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		// plugins: [basicSsl()], // bun does not support this yet
-	},
 	server: {
-		https: false, // bun does not support this yet
 		host: "0.0.0.0",
+		https: true,
 	},
 	prefetch: true,
 	compressHTML: true,
