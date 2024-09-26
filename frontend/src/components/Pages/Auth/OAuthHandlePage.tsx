30f8ff8a229df8d7d7f7fcc90a93ee98285a8774
Amir <rezaamir731@gmail.com>
2024-09-26 17:34:23 +0330

    feat: redesign o auth handler page

--- frontend/src/components/Pages/Auth/OAuthHandlePage.tsx
+++ frontend/src/components/Pages/Auth/OAuthHandlePage.tsx
@@ -1,22 +1,55 @@
 import React, { type FC, useEffect, useState } from "react";
-import authServices, { handleOAuthLogin } from "@/services/auth.ts";
-import axios from "axios";
-import type { TODO_TYPE } from "@/models/utils.ts";
+import authServices from "@/services/auth.ts";
+import { Skeleton } from "@UI/skeleton.tsx";
+import { redirect } from "@/lib/utils.ts";
+
+type OAuthStates = "init" | "service_loading" | "error" | "success";
+
+const OAuthStateComponents: { [key in OAuthStates]: FC } = {
+	service_loading: () => (
+		<Skeleton className={"flex min-h-40 items-center justify-center p-4 text-2xl"}>We're authenticating you. Please wait...</Skeleton>
+	),
+	init: () => OAuthStateComponents["service_loading"]({}),
+	error: () => {
+		useEffect(() => {
+			setTimeout(() => {
+				redirect("/auth/login");
+			}, 3000);
+		}, []);
+		return (
+			<div className={"rounded-2xl bg-red-500 p-4 text-black"}>
+				<p>Something went wrong. You will be redirected to the login page.</p>
+			</div>
+		);
+	},
+	success: () => {
+		useEffect(() => {
+			setTimeout(() => {
+				redirect("/panel");
+			}, 2000);
+		}, []);
+		return (
+			<div className={"rounded-2xl bg-green-200 p-4 text-black"}>
+				<p>Successfully Authenticated, You Will Be Redirected To Your Destination Shortly...</p>
+			</div>
+		);
+	},
+};
 
 const OAuthHandlePage: FC = () => {
-	const [handlingState, setHandlingState] = useState<TODO_TYPE>(null);
+	const [handlingState, setHandlingState] = useState<OAuthStates>("init");
 
 	useEffect(() => {
-		if (!handlingState) setHandlingState("loading");
-		if (handlingState === "loading") return;
+		if (handlingState === "service_loading") return;
+		setHandlingState("service_loading");
 		const url = new URL(window.location.href);
 		const provider = url.searchParams.get("provider");
 		const code = url.searchParams.get("code");
 		const error = url.searchParams.get("error");
 		const state = url.searchParams.get("state");
 		if (error) return setHandlingState("error");
-		if (!code || !state) return setHandlingState("error_no_state_or_code");
-		if (!provider) return setHandlingState("error_no_provider");
+		if (!code || !state) return setHandlingState("error");
+		if (!provider) return setHandlingState("error");
 		authServices
 			.handleOAuthLogin({
 				provider,
@@ -24,35 +57,15 @@ const OAuthHandlePage: FC = () => {
 				state,
 			})
 			.then(() => {
-				setHandlingState("Done!!!");
+				setHandlingState("success");
 			})
-			.catch(() => setHandlingState("ERR"));
+			.catch(() => setHandlingState("error"));
 	}, []);
+
+	const StateComponent = OAuthStateComponents[handlingState];
 	return (
 		<div>
-			handling o-auth {handlingState && handlingState}
-			<button
-				onClick={() =>
-					axios
-						.get("https://127.0.0.1:3033/auth/fuck", {
-							withCredentials: true,
-						})
-						.finally(() => setHandlingState("fucked "))
-				}
-			>
-				fuck
-			</button>
-			<button
-				onClick={() =>
-					axios
-						.get("https://127.0.0.1:3033/auth/users/user-info", {
-							withCredentials: true,
-						})
-						.catch(() => setHandlingState("Unauthorized"))
-				}
-			>
-				check cookies
-			</button>
+			<StateComponent />
 		</div>
 	);
 };
