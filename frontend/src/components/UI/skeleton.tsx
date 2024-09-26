6879fa275d76c539ec5dc4dd2d06fa28b9b07999
Amir <rezaamir731@gmail.com>
2024-09-26 17:31:25 +0330

    feat: add children render support for skeleton

--- frontend/src/components/UI/skeleton.tsx
+++ frontend/src/components/UI/skeleton.tsx
@@ -1,8 +1,12 @@
 import { cn } from "@/lib/shadcn-utils";
 import React from "react";
 
-function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-	return <div className={cn("animate-pulse rounded-md bg-muted", className)} {...props} />;
+function Skeleton({ className, children, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+	return (
+		<div className={cn("animate-pulse rounded-md bg-muted", className)} {...props}>
+			{children}
+		</div>
+	);
 }
 
 export { Skeleton };
