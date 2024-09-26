5c2bb1332512eee8907e1cb425a199088341f0a2
Amir <rezaamir731@gmail.com>
2024-09-26 17:35:39 +0330

    feat: add use init auth hook

---
+++ frontend/src/auth/index.ts
@@ -0,0 +1,41 @@
+import { useStore } from "@nanostores/react";
+import { $AuthStore } from "@/stores/auth.ts";
+import { useEffect } from "react";
+import authServices from "@/services/auth.ts";
+import { handleServiceError } from "@/lib/services.ts";
+
+export const useInitAuth = () => {
+	const { user, authError, isLoading, isAuthenticated } = useStore($AuthStore);
+	if (typeof window === "undefined") $AuthStore.setKey("isLoading", true);
+	useEffect(() => {
+		if (user || isLoading || isAuthenticated || authError) return;
+		$AuthStore.setKey("isLoading", true);
+		authServices
+			.getUserInfo()
+			.then((r) => {
+				if (!r?.data?.id) throw new Error("unknown error");
+				$AuthStore.set({
+					user: {
+						id: r.data.id,
+						email: r.data.email,
+					},
+					isAuthenticated: true,
+					isLoading: false,
+				});
+			})
+			.catch((e) => {
+				$AuthStore.set({
+					authError: handleServiceError(e),
+					isAuthenticated: false,
+				});
+			})
+			.finally(() => {
+				if (isLoading) $AuthStore.setKey("isLoading", false);
+			});
+	}, []);
+};
+
+export const useAuthStore = () => {
+	const store = useStore($AuthStore);
+	return store;
+};
