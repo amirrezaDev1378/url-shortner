96cc55e0984743ebbcc577d895f34944d552ceda
Amir <rezaamir731@gmail.com>
2024-09-27 23:50:13 +0330

    feat: add support for custom error and loading components

--- frontend/src/auth/Guard.tsx
+++ frontend/src/auth/Guard.tsx
@@ -1,22 +1,32 @@
 import React, { type FC } from "react";
 import { useAuthStore } from "@/auth/index.ts";
 import { redirect } from "@/lib/utils.ts";
+import { Skeleton } from "@UI/skeleton.tsx";
 
 export interface GuardProps {
 	children: React.ReactNode;
 	loadingComponent?: React.ReactNode;
+	errorComponent?: React.ReactNode;
 	redirectUrl?: string;
 }
 
-const AuthGuard: FC<GuardProps> = ({ redirectUrl = "/auth/login", children, loadingComponent }) => {
+const DefaultLoadingComponent: React.ReactNode = (() => <Skeleton className={"h-100svh w-full"} />)();
+const DefaultErrorComponent: React.ReactNode = (() => <div>You are not authenticated, please login again.</div>)();
+
+const AuthGuard: FC<GuardProps> = ({
+	redirectUrl = "/auth/login",
+	children,
+	loadingComponent = DefaultLoadingComponent,
+	errorComponent = DefaultErrorComponent,
+}) => {
 	const { user, authError, isAuthenticated, isLoading } = useAuthStore();
 
-	if (isLoading) return loadingComponent || <p>loading babe...</p>;
+	if (isLoading) return loadingComponent;
 	if (authError) {
 		redirect(redirectUrl);
-		return <p>Err validating you please login again</p>;
+		return errorComponent;
 	}
-	if (!isAuthenticated || !user?.id) return <p>some thing does not add up</p>;
+	if (!isAuthenticated || !user?.id) return errorComponent;
 	return children;
 };
 
