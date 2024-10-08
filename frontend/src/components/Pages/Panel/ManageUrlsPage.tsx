79a9d85b1ff1e37afeb837085eb4411e63e344b1
Amir <rezaamir731@gmail.com>
2024-10-09 00:47:50 +0330

    feat: add real data fetching

--- frontend/src/components/Pages/Panel/ManageUrlsPage.tsx
+++ frontend/src/components/Pages/Panel/ManageUrlsPage.tsx
@@ -1,49 +1,80 @@
 import * as React from "react";
-import { type FC } from "react";
+import { type FC, useEffect, useState } from "react";
 import { Button } from "@UI/button.tsx";
 import { type ColumnDef } from "@tanstack/react-table";
 import SimpleTable from "@UI/SimpleTable.tsx";
-import { LuArrowUpDown as ArrowUpDown } from "react-icons/lu";
+import { GetAllUrlsService } from "@/services/urls.ts";
+import { toast } from "@/hooks/useToast.ts";
+import { formatDateTime } from "@/lib/datetime.ts";
+import { formatShortedURL } from "@/lib/utils.ts";
+import type { GetAllUrlsResponse } from "@/models/generated.ts";
 
-interface ShortLink {
-	name: string;
-	created: Date;
-	shortLink: string;
-	totalClicks: number;
-	lastClick: number;
-}
-
-const columns: ColumnDef<ShortLink>[] = [
+const columns: ColumnDef<GetAllUrlsResponse>[] = [
 	{
-		accessorKey: "shortLink",
+		accessorKey: "slug",
 		header: "Link",
-	},
-	{
-		accessorKey: "name",
-		cell: ({ row }) => <div className="lowercase">{row.getValue("name")}</div>,
-
-		header: ({ column }) => {
-			return (
-				<Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
-					Email
-					<ArrowUpDown className="ml-2 h-4 w-4" />
-				</Button>
-			);
+		cell: (e) => {
+			const formatted: string = formatShortedURL(e.getValue() as string)?.toString() || "-";
+			return <a href={formatted}>{formatted}</a>;
 		},
 	},
-];
-const data = [
-	{ name: "asd", created: "asd" as any, lastClick: 123, totalClicks: 123, shortLink: "asd" },
 	{
-		name: "Boobs",
-		created: "asd" as any,
-		lastClick: 123,
-		totalClicks: 123,
-		shortLink: "asd",
+		accessorKey: "general_redirect_path",
+		header: "Redirect Path",
+		cell: (e) => {
+			const url: string = e.getValue() as string;
+			if (!url) return <p>-</p>;
+			return <a href={url}>{url}</a>;
+		},
+	},
+	{
+		accessorKey: "ios_redirect_path",
+		header: "IOS Redirect Path",
+		cell: (e) => {
+			const url: string = e.getValue() as string;
+			if (!url) return <p>-</p>;
+			return <a href={url}>{url}</a>;
+		},
+	},
+	{
+		accessorKey: "created_at",
+		header: "Creation Date",
+		cell: (e) => formatDateTime({ date: e.getValue(), fallback: "-" }),
 	},
 ];
 
 const ManageUrlsPage: FC = () => {
+	const [urlsServiceData, setUrlsServiceData] = useState<{
+		error?: string;
+		isLoading?: boolean;
+		data?: GetAllUrlsResponse;
+	}>({
+		error: "",
+		isLoading: true,
+		data: [],
+	});
+	useEffect(() => {
+		setUrlsServiceData({ isLoading: true });
+		GetAllUrlsService()
+			.then((r) => {
+				setUrlsServiceData({
+					error: "",
+					data: r.data || [],
+				});
+			})
+			.catch((e) => {
+				setUrlsServiceData({ error: e });
+				toast({
+					variant: "destructive",
+					title: "Something Went Wrong While getting URLs List...",
+					description: <Button onClick={(e) => window.location.reload()}>Try To Refresh Page.</Button>,
+				});
+			})
+			.finally(() => {
+				setUrlsServiceData((prevState) => ({ ...prevState, isLoading: false }));
+			});
+	}, []);
+
 	return (
 		<div className={"flex w-full flex-col p-6"}>
 			<h3 className={"mt-5 w-full text-left text-3xl"}>Manage Your Shorted Links.</h3>
@@ -54,7 +85,7 @@ const ManageUrlsPage: FC = () => {
 					<a href="/panel/create">Create New Link</a>
 				</Button>
 			</div>
-			<SimpleTable pagination data={data} columns={columns} />
+			<SimpleTable pagination data={urlsServiceData.data as GetAllUrlsResponse} columns={columns} />
 		</div>
 	);
 };
