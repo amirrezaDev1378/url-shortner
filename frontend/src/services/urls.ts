64d40cd0b0c1c6104e1e827fe0ad8f8c8b6b6eb4
Amir <rezaamir731@gmail.com>
2024-10-08 23:59:24 +0330

    feat: add create url service

---
+++ frontend/src/services/urls.ts
@@ -0,0 +1,22 @@
+import { appRequest } from "@/lib/request.ts";
+import type { CreateUrlRequest, CreateUrlResponse } from "@/models/generated.ts";
+import { handleServiceError, type ServiceResponse } from "@/lib/services.ts";
+import type { AxiosResponse } from "axios";
+
+const CreateUrlService = async (params: CreateUrlRequest): Promise<ServiceResponse<CreateUrlResponse>> => {
+	const res = await appRequest
+		.post<CreateUrlRequest, AxiosResponse<CreateUrlResponse>>("/urls/create", {
+			general_redirect_path: params.general_redirect_path,
+			ios_redirect_path: params.ios_redirect_path,
+			type: params.type,
+			expiration: params.expiration,
+		})
+		.catch((e) => e);
+
+	const error = handleServiceError(res);
+	if (error) return { error };
+
+	return { data: res.data };
+};
+
+export { CreateUrlService };
