0335b618fcd86a986f74f15207ce4505c166649e
Amir <rezaamir731@gmail.com>
2024-11-05 02:00:35 +0330

    feat: add todo alert for analytics page

--- frontend/src/components/Pages/Panel/AnalyticUrlsPage.tsx
+++ frontend/src/components/Pages/Panel/AnalyticUrlsPage.tsx
@@ -1,28 +1,44 @@
 import * as React from "react";
 import { type FC } from "react";
 import {
-	AnalyticUrlsTotalClick,
-	AnalyticUrlsByLocation,
 	AnalyticsUrlsByDevice,
+	AnalyticUrlsByLocation,
+	AnalyticUrlsTotalClick,
 	TopUrlsAnalytics,
 } from "./components/AnalyticUrlsComponents.tsx";
+import { cn } from "@/lib/shadcn-utils.ts";
+
+const TODO = true;
 
 const AnalyticUrlsPage: FC = () => {
 	return (
-		<div className={"flex flex-col w-full p-6"}>
-			<h3 className={"text-left text-3xl mt-5 w-full"}>Analytics.</h3>
+		<div className={cn("flex w-full flex-col p-6", TODO && "pointer-events-none")}>
+			{TODO && (
+				<div
+					className={
+						"absolute inset-0 z-[50000] ml-auto flex w-[calc(100%-12rem)] items-center justify-center bg-black/30 backdrop-blur-lg"
+					}
+				>
+					<h1 className={"text-3xl"}>
+						Sorry but this page is still under development🥲,
+						<br />
+						Please come back later, We're working on it!
+					</h1>
+				</div>
+			)}
+			<h3 className={"mt-5 w-full text-left text-3xl"}>Analytics.</h3>
 			<hr className={"my-4 w-full"} />
-			<div className={"flex flex-row flex-wrap  justify-between "}>
-				<div className="md:w-[46%] p-8 shadow-sm-center shadow-main rounded-2xl m-[12px]]">
+			<div className={"flex flex-row flex-wrap justify-between"}>
+				<div className="m-[12px]] rounded-2xl p-8 shadow-sm-center shadow-main md:w-[46%]">
 					<AnalyticUrlsTotalClick />
 				</div>
-				<div className="md:w-[46%] p-8 shadow-sm-center shadow-main rounded-2xl m-[12px]]">
+				<div className="m-[12px]] rounded-2xl p-8 shadow-sm-center shadow-main md:w-[46%]">
 					<AnalyticUrlsByLocation />
 				</div>
-				<div className="md:w-[46%] p-8 shadow-sm-center shadow-main rounded-2xl m-[12px]] mt-12">
+				<div className="m-[12px]] mt-12 rounded-2xl p-8 shadow-sm-center shadow-main md:w-[46%]">
 					<AnalyticsUrlsByDevice />
 				</div>
-				<div className="md:w-[46%] p-8 shadow-sm-center shadow-main rounded-2xl m-[12px]] mt-12">
+				<div className="m-[12px]] mt-12 rounded-2xl p-8 shadow-sm-center shadow-main md:w-[46%]">
 					<TopUrlsAnalytics />
 				</div>
 			</div>
