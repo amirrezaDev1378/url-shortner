4fb22e30b3ef070a3235ca17a02e1fefa475fcd0
Amir <rezaamir731@gmail.com>
2024-11-05 01:49:52 +0330

    feat: add url actions(delete)

--- frontend/src/components/Pages/Panel/ManageUrlsPage.tsx
+++ frontend/src/components/Pages/Panel/ManageUrlsPage.tsx
@@ -3,13 +3,28 @@ import { type FC, useEffect, useState } from "react";
 import { Button } from "@UI/button.tsx";
 import { type ColumnDef } from "@tanstack/react-table";
 import SimpleTable from "@UI/SimpleTable.tsx";
-import { GetAllUrlsService } from "@/services/urls.ts";
+import { DeleteUrlService, GetAllUrlsService } from "@/services/urls.ts";
 import { toast } from "@/hooks/useToast.ts";
 import { formatDateTime } from "@/lib/datetime.ts";
 import { convertShortedURLsToDate } from "@/lib/utils.ts";
-import type { GetAllUrlsResponse } from "@/models/generated.ts";
+import type { GetAllUrlsResponse, UrlResponse } from "@/models/generated.ts";
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+} from "@UI/alert-dialog.tsx";
 
 const columns: ColumnDef<GetAllUrlsResponse>[] = [
+	{
+		accessorKey: "created_at",
+		header: "Creation Date",
+		cell: (e) => formatDateTime({ date: e.getValue(), fallback: "-" }),
+	},
 	{
 		accessorKey: "slug",
 		header: "Link",
@@ -36,11 +51,6 @@ const columns: ColumnDef<GetAllUrlsResponse>[] = [
 			return <a href={url}>{url}</a>;
 		},
 	},
-	{
-		accessorKey: "created_at",
-		header: "Creation Date",
-		cell: (e) => formatDateTime({ date: e.getValue(), fallback: "-" }),
-	},
 ];
 
 const ManageUrlsPage: FC = () => {
@@ -53,7 +63,29 @@ const ManageUrlsPage: FC = () => {
 		isLoading: true,
 		data: [],
 	});
-	useEffect(() => {
+
+	const [deleteUrlModalInfo, setDeleteUrlModalInfo] = useState({ isOpen: false, id: "" });
+
+	const handleDeleteUrl = (id: string) => () => {
+		setDeleteUrlModalInfo({ isOpen: true, id });
+	};
+
+	const onDeleteUrlConfirm = (id: string) => {
+		DeleteUrlService({ id })
+			.then((r) => {
+				if (r.data?.success) {
+					toast({ variant: "default", title: "URL Deleted Successfully!" });
+					setDeleteUrlModalInfo({ isOpen: false, id: "" });
+					return fetchUrlsData();
+				}
+				throw r;
+			})
+			.catch((e) => {
+				toast({ variant: "destructive", title: "Something went wrong while deleting the URL..." });
+				console.error(e);
+			});
+	};
+	const fetchUrlsData = () => {
 		setUrlsServiceData({ isLoading: true });
 		GetAllUrlsService()
 			.then((r) => {
@@ -67,13 +99,31 @@ const ManageUrlsPage: FC = () => {
 				toast({
 					variant: "destructive",
 					title: "Something went wrong while getting URLs list...",
-					description: <Button onClick={(e) => window.location.reload()}>Try To Refresh Page.</Button>,
+					description: <Button onClick={() => window.location.reload()}>Try To Refresh Page.</Button>,
 				});
 			})
 			.finally(() => {
 				setUrlsServiceData((prevState) => ({ ...prevState, isLoading: false }));
 			});
+	};
+	useEffect(() => {
+		fetchUrlsData();
 	}, []);
+
+	const tableActions: ColumnDef<UrlResponse>[] = [
+		{
+			header: "#",
+			cell: (e) => {
+				return (
+					<div>
+						<Button onClick={handleDeleteUrl(e.row.original.id)} variant={"destructive"}>
+							Delete
+						</Button>
+					</div>
+				);
+			},
+		},
+	];
 
 	return (
 		<div className={"flex w-full flex-col p-6"}>
@@ -85,9 +135,25 @@ const ManageUrlsPage: FC = () => {
 					<a href="/panel/create">Create New Link</a>
 				</Button>
 			</div>
-			<SimpleTable pagination data={urlsServiceData.data as GetAllUrlsResponse} columns={columns} />
+			<SimpleTable pagination data={urlsServiceData.data as GetAllUrlsResponse} columns={[...columns, ...tableActions]} />
+
+			<AlertDialog
+				open={deleteUrlModalInfo.isOpen}
+				defaultOpen={false}
+				onOpenChange={(state) => setDeleteUrlModalInfo({ isOpen: state, id: "" })}
+			>
+				<AlertDialogContent>
+					<AlertDialogHeader>
+						<AlertDialogTitle>Are you absolutely sure to delete that this url? </AlertDialogTitle>
+						<AlertDialogDescription>This action can not be undone!</AlertDialogDescription>
+					</AlertDialogHeader>
+					<AlertDialogFooter>
+						<AlertDialogCancel>Cancel</AlertDialogCancel>
+						<AlertDialogAction onClick={() => onDeleteUrlConfirm(deleteUrlModalInfo.id)}>Continue</AlertDialogAction>
+					</AlertDialogFooter>
+				</AlertDialogContent>
+			</AlertDialog>
 		</div>
 	);
 };
-
 export default ManageUrlsPage;
