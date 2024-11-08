97af15fd85a6525213282482d4d4a33da3cbf21c
Amir <rezaamir731@gmail.com>
2024-11-08 23:43:07 +0330

    feat: -add avatar and name to user info response model

--- backend/app/models/auth.go
+++ backend/app/models/auth.go
@@ -1,6 +1,8 @@
 package models
 
 type UserInfoResponse struct {
-	ID    string `json:"id"`
-	Email string `json:"email"`
+	ID     string `json:"id"`
+	Email  string `json:"email"`
+	Avatar string `json:"avatar"`
+	Name   string `json:"name"`
 }
