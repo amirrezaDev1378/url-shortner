5bcbfa34ef2efb00df37aed7e52327f64020aac5
Amir <rezaamir731@gmail.com>
2024-11-09 00:18:10 +0330

    feat: add auth store actions

--- frontend/src/stores/auth.ts
+++ frontend/src/stores/auth.ts
@@ -1,15 +1,37 @@
 import { map } from "nanostores";
 import type { ServiceError } from "@/lib/services.ts";
+import authServices from "@/services/auth.ts";
 
 interface AuthStoreParams {
 	user?: {
 		id: string;
 		email: string;
+		avatar?: string;
+		name?: string;
 	};
 	isLoading?: boolean;
 	isAuthenticated?: boolean;
 	authError?: Partial<ServiceError>;
 }
+
+export interface AuthStoreActions {
+	logout: () => Promise<boolean>;
+}
+
+export const $AuthStoreActions: AuthStoreActions = {
+	logout: async (): Promise<boolean> => {
+		try {
+			await authServices.logoutUser();
+			$AuthStore.set({
+				isLoading: false,
+				isAuthenticated: false,
+			});
+			return true;
+		} catch (e) {
+			return false;
+		}
+	},
+};
 
 const $AuthStore = map<AuthStoreParams>({} as AuthStoreParams);
 
