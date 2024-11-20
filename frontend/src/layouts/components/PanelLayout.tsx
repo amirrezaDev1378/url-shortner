30bd7b227f0321edb2a10a0af583b3cde2c08e54
Amir <rezaamir731@gmail.com>
2024-11-21 00:46:05 +0330

    feat: adjust styles

--- frontend/src/layouts/components/PanelLayout.tsx
+++ frontend/src/layouts/components/PanelLayout.tsx
@@ -3,9 +3,9 @@
 import React, { type FC, type ReactNode, useState } from "react";
 import { RxHamburgerMenu as HamburgerMenuIcon } from "react-icons/rx";
 import { cn } from "@/lib/shadcn-utils.ts";
-import AuthProvider from "@/auth/Provider.tsx";
 import AuthGuard from "@/auth/Guard.tsx";
 import { Skeleton } from "@UI/skeleton.tsx";
+import { UserMenu } from "@/components/Pages/General/Layouts/Main.tsx";
 
 interface Props {
 	children: ReactNode | ReactNode[];
@@ -48,21 +48,36 @@ const PanelLayout: FC<Props> = ({ children, currentRoute }) => {
 	return (
 		<AuthGuard loadingComponent={<PanelLoadingComponent />}>
 			<div className="flex h-full w-full flex-col md:flex-row">
-				<div id="mobile-header" className="h-[50px] w-full justify-between md:hidden">
-					<HamburgerMenuIcon onClick={toggleMenu} id="hamburger-menu" className="cursor-pointer" />
+				<div id="mobile-header" className="flex h-[50px] w-full flex-row items-center justify-between px-3 pt-3 md:hidden">
+					<div className={"flex flex-row items-center gap-3"}>
+						<HamburgerMenuIcon onClick={toggleMenu} id="hamburger-menu" className="cursor-pointer text-3xl" />
+						<a href={"/"} className={""}>
+							<img alt={"UOS"} src={"/logo/logo-light.png"} className={"w-14"} />
+						</a>
+					</div>
+					<UserMenu contentProps={{ align: "start" }} />
 				</div>
 
 				<div onClick={toggleMenu} className={cn({ lightbox: menuVisible }, "md:hidden")} />
-				<div id="menu" className={"relative md:ml-4 md:mr-4 md:mt-6 md:w-44"}>
+				<div id="menu" className={"md:ml-4 md:mr-4 md:mt-6 md:w-44"}>
 					<div
 						className={cn(
-							`absolute top-0 flex h-full flex-col items-start gap-2 transition-all md:fixed md:left-0 md:w-44 md:border-r md:border-solid md:border-neutral-700 md:px-4 md:pt-6 md:transition-none`,
+							"absolute top-0 z-50 flex h-svh flex-col items-start gap-4 bg-neutral-900 px-10 pt-4 transition-all",
+							"md:fixed md:left-0 md:w-44 md:border-r md:border-solid md:border-neutral-700",
+							"md:px-4 md:pt-6 md:transition-none",
 							{
 								"left-0": menuVisible,
 								"left-[-100%]": !menuVisible,
 							}
 						)}
 					>
+						<div className={"hidden w-full items-center justify-between md:flex"}>
+							<a href={"/"} className={""}>
+								<img alt={"UOS"} src={"/logo/logo-light.png"} className={"w-14"} />
+							</a>
+							<UserMenu contentProps={{ align: "start" }} />
+						</div>
+
 						<LinkItem currentRoute={currentRoute} href={"/panel/manage"} title={"manage"} />
 						<LinkItem currentRoute={currentRoute} href={"/panel/create"} title={"create"} />
 						<LinkItem currentRoute={currentRoute} href={"/panel/analytics"} title={"analytics"} />
